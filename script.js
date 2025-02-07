61bc6fb3bcd544faf97b14fd81314adf81ccf091
Adilzhan <142014375+adilzhanIS7@users.noreply.github.com>
2025-02-07 07:31:18 +0500

    Update script.js

--- script.js
+++ script.js
@@ -1,14 +1,14 @@
 const messages = [
-    "Are you sure?",
-    "Really sure??",
-    "Are you positive?",
-    "Pookie please...",
-    "Just think about it!",
-    "If you say no, I will be really sad...",
-    "I will be very sad...",
-    "I will be very very very sad...",
-    "Ok fine, I will stop asking...",
-    "Just kidding, say yes please! ❤️"
+    "Ты уверена?",
+    "Точно уверена??",
+    "Может подумаешь еще?",
+    "Пожалуйста пожалуйста!",
+    "Просто подумай еще чуть чуть!",
+    "Если скажешь нет, я буду очень расстроен...",
+    "Я буду очень расстроен...",
+    "Я буду очень очень расстроен...",
+    "Окей хорошо, я перестану...",
+    "Шучу, просто скажи да пожалуйста ❤️"
 ];
 
 let messageIndex = 0;
